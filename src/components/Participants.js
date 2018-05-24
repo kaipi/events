931c7abc1d7ca89f0d6742e3e6df492b13c78252
Timo Kaipiainen <timo.kaipiainen@nordcloud.com>
2018-05-24 23:22:20 +0300

    one dirty fix again

--- src/components/Participants.js
+++ src/components/Participants.js
@@ -45,72 +45,78 @@ class Participants extends Component {
   getGroups(json) {
     let groups = [];
     json.forEach(group => {
-      groups.push(
-        <a className="groups-list" href={"#" + group[0].group}>
-          {group[0].group}
-        </a>
-      );
+      if(group[0] !== undefined) {
+        groups.push(
+          <a className="groups-list" href={"#" + group[0].group}>
+            {group[0].group}
+          </a>
+        );
+      }
     });
     return groups;
   }
   getRowElements(json) {
-    let ret = [];
+    let ret = [];    
     json.forEach(group => {
-      let arr = [];
-      group.forEach(item => {
-        arr.push(
-          <tr key={item.id}>
-            <td>
-              {item.firstname} {item.lastname}
-            </td>
-            <td>{item.club}</td>
-            <td>{item.team}</td>
-            <td>{item.group}</td>
-            <td>{item.number}</td>
-            <td>
-              {this.props.loggedin ? (
-                <div>
-                  <Button
-                    id={item.id}
-                    className="app-icon-button"
-                    onClick={() => {
-                      this.removeParticipant(item.id);
-                    }}
-                    icon="trash"
-                  />
-                  {item.payment_confirmed ? (
-                    <Icon className="paid-icon" icon="tick-circle" />
-                  ) : (
-                    <Icon className="paid-icon" icon="error" />
-                  )}
-                </div>
-              ) : (
-                ""
-              )}
-            </td>
-          </tr>
-        );
-      });
-      ret.push(
-        <div>
-          <h5 className="participant-table-header">
-            <a name={group[0].group} >Sarja: {group[0].group}</a>
-          </h5>
-          <table className="pt-html-table pt-interactive event-table">
-            <thead>
-              <tr>
-                <th>Nimi</th>
-                <th>Seura</th>
-                <th>Joukkue</th>
-                <th>Sarja</th>
-                <th>Alustava kilpailunumero</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>{arr}</tbody>
-          </table>
-        </div>
-      );
+      if(group !== undefined ) {
+        let arr = [];
+        group.forEach(item => {
+          arr.push(
+            <tr key={item.id}>
+              <td>
+                {item.firstname} {item.lastname}
+              </td>
+              <td>{item.club}</td>
+              <td>{item.team}</td>
+              <td>{item.group}</td>
+              <td>{item.number}</td>
+              <td>
+                {this.props.loggedin ? (
+                  <div>
+                    <Button
+                      id={item.id}
+                      className="app-icon-button"
+                      onClick={() => {
+                        this.removeParticipant(item.id);
+                      }}
+                      icon="trash"
+                    />
+                    {item.payment_confirmed ? (
+                      <Icon className="paid-icon" icon="tick-circle" />
+                    ) : (
+                      <Icon className="paid-icon" icon="error" />
+                    )}
+                  </div>
+                ) : (
+                  ""
+                )}
+              </td>
+            </tr>
+          );
+        });
+        if(group[0] !== undefined) {
+          ret.push(
+            <div>
+              <h5 className="participant-table-header">
+                <a name={group[0].group} >Sarja: {group[0].group}</a>
+              </h5>
+              <table className="pt-html-table pt-interactive event-table">
+                <thead>
+                  <tr>
+                    <th>Nimi</th>
+                    <th>Seura</th>
+                    <th>Joukkue</th>
+                    <th>Sarja</th>
+                    <th>Alustava kilpailunumero</th>
+                    <th />
+                  </tr>
+                </thead>
+                <tbody>{arr}</tbody>
+              </table>
+            </div>
+          );
+        }
+      }
     });
     return ret;
   }
